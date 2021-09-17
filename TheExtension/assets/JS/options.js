85ff7ff91361b33f51dc9f44e4fee86b17d0e755
Yasir Arfat <38040354+coddleflop@users.noreply.github.com>
2021-09-17 08:36:02 +0600

    csv export import

--- TheExtension/assets/JS/options.js
+++ TheExtension/assets/JS/options.js
@@ -3,6 +3,9 @@ var OnDatabase = document.getElementById("database");
 var Save = document.getElementById("save-btn");
 var Reset = document.getElementById("reset-btn");
 var Export = document.getElementById("Export");
+var CSV = document.getElementById("export-btn");
+var ImportBtn = document.getElementById("import-btn");
+var importFromCSV = document.getElementById("importFromCSV");
 var DBuri = document.getElementById("databaseuri");
 var CrlHistory = document.getElementById("clear-history");
 var Sound = document.getElementById("sound");
@@ -93,13 +96,65 @@ Export.onclick = function (e) {
 
 }
 
+CSV.onclick = function (e) {
+        e.preventDefault();
 
+        chrome.storage.sync.get({ 'Words': [], 'db': '' }, function (data) {
+                var Words = data.Words;
+                let obj = '';
+                Words.forEach(word => {
+                        obj += word.Word + "," + word.Definition + "," + word.Translation + "\n";
+                });
+                var downloadLink = document.createElement("a");
+                var blob = new Blob(["\ufeff", obj]);
+                var url = URL.createObjectURL(blob);
+                downloadLink.href = url;
+                downloadLink.download = "Words.csv";
+                document.body.appendChild(downloadLink);
+                downloadLink.click();
+                document.body.removeChild(downloadLink);
+                flash('Exported all data to csv');
+        });
+
+}
 
 CrlHistory.onclick = function (e) {
         e.preventDefault();
         if (confirm("You are going to clear all the words saved locally. Continue?")) {
                 chrome.storage.sync.remove('Words', () => flash("All saved words are cleared from cache"));
         }
+};
+
+ImportBtn.onclick = function (e) {
+        e.preventDefault();
+        importFromCSV.click();
+};
+
+
+importFromCSV.onchange = function (e) {
+        e.preventDefault();
+        if(!confirm("The Extension Expects CSV to have three Value Tuples\n\n\
+Word         Definition     Translation\n\n\
+Other values will be discarded, Values can be empty \n\n\
+Other arrengements aren't supported! Please make sure.")) return;
+        var reader = new FileReader();
+        reader.addEventListener("load", function () {
+        allText = this.result;
+        allTextLines = allText.split(/\r\n|\n/);
+        chrome.storage.sync.get({ 'Words': [] }, function (data) {
+                let obj = data.Words;
+                let time = new Date().toISOString().slice(0, 10) + " " + new Date().toISOString().slice(11, 19);
+                allTextLines.forEach(element => {
+                        entries = element.split(',');
+                        if(entries.length>1)
+                        obj.push({ Serial: obj.length + 1, Word: entries[0], Definition: entries[1], Translation: entries[2], SaveTime: time });
+                });
+                chrome.storage.sync.set({ 'Words': obj }, () => flash('CSV Data imported successfuly'));
+        });
+        
+        });
+        reader.readAsText(importFromCSV.files.item(0));
+        
 };
 
 //on options page, load the settings
